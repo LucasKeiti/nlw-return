dcac6ae667eda5b471f038228bad085c5a168442
Lucas Keiti <lucaskeiti2010@gmail.com>
2022-05-07 15:53:04 -0300

    Update styles and add new functions for menu buttons

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ function onScroll() {
         navigation.classList.remove('scroll')
     }
 }
+
+function openMenu(){
+    document.body.classList.add('menu-expanded')
+
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu-expanded')
+}
