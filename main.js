70d9cf1e2b67df1b8b290f9499ac19a92364709e
Lucas Keiti <lucaskeiti2010@gmail.com>
2022-05-10 00:48:19 -0300

    Added footer section, contacts, address and back to top button

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 function onScroll() {
-    if (scrollY > 0) {
-        navigation.classList.add('scroll')
-    } else {
-        navigation.classList.remove('scroll')
-    }
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
 }
+
+function showNavOnScroll() {
+    if(scrollY > 0) {
+        navigation.classList.add('scroll')
+        } else {
+            navigation.classList.remove('scroll')
+        }
+    }
+
+    function showBackToTopButtonOnScroll() {
+        if(scrollY > 500) {
+            navigation.classList.add ('show')
+            } else {
+                navigation.classList.remove('show')
+            }
+        }
 
 function openMenu(){
     document.body.classList.add('menu-expanded')
