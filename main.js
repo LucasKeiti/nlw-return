cda6ca4dd4a4d4abec20ce6485b1e66c389a113a
Lucas Keiti <lucaskeiti2010@gmail.com>
2022-05-20 22:20:51 -0300

    Update some classes in index.html and added a new activate function for all sections

--- main.js
+++ main.js
@@ -4,6 +4,43 @@ onScroll()
 function onScroll() {
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    //activateMenuAtCurrentSection(services)
+}
+
+function activateMenuAtCurrentSection(section){
+    const targetLine = scrollY + innerHeight /2
+
+    //Verificar se a seção passou da linha
+    //quais dados vou precisar?
+
+    //o topo da seção
+    const sectionTop = section.offsetTop
+    //a altura da seção
+    const sectionHeight = section.offsetHeight
+
+    //o topo das seção chegou ou ultrapassou a linha alvo
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+    //informações dos dados e da lógica 
+    console.log(sectionTopReachOrPassedTargetline)
+
+    //verificar se a base está abaixo da linha alvo
+    //quais dados vou precisar?
+
+    //a seção termina onde?
+    const sectionEndsAt = sectionTop + sectionHeight
+    
+    //o final da seção passou da linha alvo
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+    //limites da seção
+    const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+    if(sectionBoundaries) {
+        console.log('estou na seção HOME')
+    }
+
 }
 
 function showNavOnScroll() {
